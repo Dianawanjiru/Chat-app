ec55383e3ecf8ea512a4a1e07a32203d4d0f6bbb
Diana <diana.wanjiru@student.moringaschool.com>
2022-10-07 10:18:36 +0300

    build components here

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+  
+import React from "react";
+import   "./App.css";
+import Header from "./Header";
+import AddContact from "./addContact";
+import ContactList from "./contactList";
+
+
+  function App(){
+    return (
+        <div>Hello World</div>
+    )
+    
+  }
+
+  export default App;
