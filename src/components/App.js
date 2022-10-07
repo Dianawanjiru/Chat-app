29c0d306da539006688305b3d333157de24105e7
Diana <diana.wanjiru@student.moringaschool.com>
2022-10-07 12:57:35 +0300

    add the contact functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
   
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import   "./App.css";
 import Header from "./Header";
 import AddContact from "./addContact";
@@ -7,8 +8,38 @@ import ContactList from "./contactList";
 
 
   function App(){
+    const LOCAL_STORAGE_KEY = "contacts";
+    const [contacts, setContacts] = useState(
+        JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) ?? []
+      );
+      const addContactHandler = (contact) => {
+        console.log(contact);
+        setContacts([...contacts, { id: uuid(), ...contact }]);
+      };
+    
+      const removeContactHandler = (id) => {
+        const newContactList = contacts.filter((contact) => {
+          return contact.id !== id;
+        });
+    
+        setContacts(newContactList);
+      };
+    
+      // useEffect(() => {
+      //   const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+      //   if (retriveContacts) setContacts(retriveContacts);
+      // }, []);
+    
+      useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+      }, [contacts]);
+
     return (
-        <div>Hello World</div>
+        <div>
+            <Header/>
+            <AddContact addContactHandler={addContactHandler}/>
+            <ContactList contacts={contacts} getContactId={removeContactHandler}/>
+        </div>
     )
     
   }
